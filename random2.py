0c9bd5ed5a7261b148ac9943abd7dd96167b733f
Curry Xu <jialincurryxu@gmail.com>
2019-05-07 16:32:46 -0700

    Create random2.py

---
+++ random2.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+print ("This is another random python script used for testing branching in git for CMPM120")
+print ("OKay we need to make it different")
+print ("weeeeeeee!")
